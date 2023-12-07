987f53ebb81f0c166b82e6ca9c9156bc1911d470
nhpd <dmitry.klpv@gmail.com>
2023-12-07 13:47:49 +0400

    extract set_wormhole_core into function

--- contracts/isms/wormhole/src/contract.rs
+++ contracts/isms/wormhole/src/contract.rs
@@ -4,7 +4,6 @@ use crate::wormhole::{ParsedVAA, WormholeQueryMsg};
 use crate::{ContractError, CONTRACT_NAME, CONTRACT_VERSION, WORMHOLE_CORE};
 use cosmwasm_std::{
     ensure_eq, Binary, Deps, DepsMut, Empty, Env, HexBinary, MessageInfo, QueryResponse, Response,
-    StdResult,
 };
 use cw2::set_contract_version;
 use hpl_interface::ism::wormhole::{ExecuteMsg, InstantiateMsg, QueryMsg, WormholeIsmQueryMsg};
@@ -46,20 +45,8 @@ pub fn execute(
     match msg {
         ExecuteMsg::Ownable(msg) => Ok(hpl_ownable::handle(deps, env, info, msg)?),
         ExecuteMsg::SetWormholeCore { wormhole_core } => {
-            ensure_eq!(
-                hpl_ownable::get_owner(deps.storage)?,
-                info.sender,
-                ContractError::Unauthorized
-            );
-
-            let wormhole_core = deps.api.addr_validate(&wormhole_core)?;
-            WORMHOLE_CORE.save(deps.storage, &wormhole_core)?;
-
-            Ok(Response::new().add_event(
-                new_event("set_wormhole_core").add_attribute("wormhole_core", wormhole_core),
-            ))
+            handle_set_wormhole_core(deps, info, wormhole_core)
         }
-
         // metadata is actually VAA data in order for it to work
         ExecuteMsg::SubmitMeta { metadata, message } => handle_submit_meta(deps, metadata, message),
     }
@@ -86,6 +73,37 @@ pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> Result<QueryResponse, Contr
             )?),
         },
     }
+}
+
+fn handle_set_wormhole_core(
+    deps: DepsMut,
+    info: MessageInfo,
+    wormhole_core: String,
+) -> Result<Response, ContractError> {
+    ensure_eq!(
+        hpl_ownable::get_owner(deps.storage)?,
+        info.sender,
+        ContractError::Unauthorized
+    );
+
+    let wormhole_core = deps.api.addr_validate(&wormhole_core)?;
+    WORMHOLE_CORE.save(deps.storage, &wormhole_core)?;
+
+    Ok(Response::new()
+        .add_event(new_event("set_wormhole_core").add_attribute("wormhole_core", wormhole_core)))
+}
+
+fn handle_submit_meta(
+    deps: DepsMut,
+    metadata: HexBinary,
+    message: HexBinary,
+) -> Result<Response, ContractError> {
+    // unpack and verify vaa and check that the message is indeed (indeed what?)
+    let packed_id = unpack_verify_vaa(deps.as_ref(), metadata, message)?;
+
+    VERIFIED_IDS.save(deps.storage, packed_id.into(), &())?;
+
+    Ok(Response::default().add_event(new_event("submit_meta")))
 }
 
 /// **unpack_verify_vaa** uses core wormhole contract to verify and unpack the vaa inside metadata
@@ -119,19 +137,6 @@ fn unpack_verify_vaa(
     Ok(packed_id)
 }
 
-fn handle_submit_meta(
-    deps: DepsMut,
-    metadata: HexBinary,
-    message: HexBinary,
-) -> Result<Response, ContractError> {
-    // unpack and verify vaa and check that the message is indeed (indeed what?)
-    let packed_id = unpack_verify_vaa(deps.as_ref(), metadata, message)?;
-
-    VERIFIED_IDS.save(deps.storage, packed_id.into(), &())?;
-
-    Ok(Response::default())
-}
-
 fn verify(
     deps: Deps,
     metadata: HexBinary,
@@ -150,6 +155,7 @@ fn verify(
     Ok(VerifyResponse { verified: true })
 }
 
+// TODO
 fn verify_info(deps: Deps, _message: HexBinary) -> Result<VerifyInfoResponse, ContractError> {
     // this is not entirely correct, but I don't see a better way to do this
     // we cannot query validators from Wormhole Core contract
