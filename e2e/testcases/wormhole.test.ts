46fc8228d911b749b3658695bfa6c6c3eda706b6
nhpd <dmitry.klpv@gmail.com>
2023-12-12 20:27:56 +0400

    e2e

--- e2e/testcases/wormhole.test.ts
+++ e2e/testcases/wormhole.test.ts
@@ -11,32 +11,16 @@ import { describe, expect, it, beforeAll, afterAll } from 'vitest';
 // import { waitFor } from '../src/helpers/sleep';
 // import { GaiaClient } from '../src/helpers/gaia_client';
 import { ethers } from 'ethers';
-// import { formatUnits, parseUnits } from '@ethersproject/units';
 import { NodeHttpTransport } from '@improbable-eng/grpc-web-node-http-transport';
-// import {
-//   ASSOCIATED_TOKEN_PROGRAM_ID,
-//   createAssociatedTokenAccountInstruction,
-//   getAssociatedTokenAddress,
-//   TOKEN_PROGRAM_ID,
-// } from '@solana/spl-token';
-// import {
-//   Connection,
-//   Keypair,
-//   PublicKey,
-//   TokenAccountsFilter,
-//   Transaction,
-// } from '@solana/web3.js';
+import { Implementation__factory } from '@certusone/wormhole-sdk/lib/esm/ethers-contracts';
 // import { publicrpc } from '@certusone/wormhole-sdk-proto-web';
-// import { BigNumber, ContractReceipt } from 'ethers';
 // const { GrpcWebImpl, PublicRPCServiceClientImpl } = publicrpc;
 
-export const ETH_NODE_URL = 'ws://localhost:8545';
+const ETH_RPC_URL = 'http://localhost:8545';
 export const ETH_PRIVATE_KEY =
   '0x6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1'; // account 1
 export const ETH_CORE_BRIDGE_ADDRESS =
   '0xC89Ce4735882C9F0f0FE26686c53074E09B0D550';
-export const ETH_NFT_BRIDGE_ADDRESS =
-  '0x26b4afb60d6c903165150c6f0aa14f8016be4aec';
 
 // const SERVER_URL = 'http://localhost:';
 // const CCQ_SERVER_URL = SERVER_URL + '6069/v1';
@@ -48,7 +32,6 @@ export const ETH_NFT_BRIDGE_ADDRESS =
 
 import {
   // approveEth,
-  attestFromEth,
   // CHAIN_ID_ETH,
   // CHAIN_ID_SOLANA,
   CHAINS,
@@ -58,14 +41,14 @@ import {
   getSignedVAAWithRetry,
   // getForeignAssetSolana,
   // getIsTransferCompletedSolana,
-  parseSequenceFromLogEth,
+  parseSequenceFromLogEth, parseVaa,
   // postVaaSolana,
   // redeemOnSolana,
   // transferFromEth,
   // tryNativeToUint8Array,
 } from '@certusone/wormhole-sdk';
 
-const WORMHOLE_RPC_URLS = ['http://localhost:7071']; // TODO: what is that?
+const WORMHOLE_RPC_URLS = ['http://localhost:7071'];
 
 describe('Test Wormhole ISM', () => {
   // const context: { park?: Cosmopark } = {};
@@ -143,34 +126,54 @@ describe('Test Wormhole ISM', () => {
     // expect(claimerAddress).toBeTruthy()
   }, 1000000);
 
-  it('kek', async () => {
+  it('publishes the message', async () => {
     // create a signer for Eth
-    const provider = new ethers.WebSocketProvider(ETH_NODE_URL);
+    const provider = new ethers.providers.WebSocketProvider(ETH_RPC_URL);
     const signer = new ethers.Wallet(ETH_PRIVATE_KEY, provider);
-    // attest the test token
-    const receipt = await attestFromEth(
-      CONTRACTS.DEVNET.ethereum.token_bridge,
+
+    console.log('signer created');
+    const wormhole = Implementation__factory.connect(
+      CONTRACTS.DEVNET.ethereum.core,
       signer,
-      TEST_ERC20,
     );
-    // get the sequence from the logs (needed to fetch the vaa)
+    console.log('connected to wormhole');
+    const payload = '0x123123';
+    const msgTx = await wormhole.publishMessage(0, payload, 1);
+    console.log('published msgTx');
+    console.log('msgTx: \n' + msgTx.raw + '\n\n\n');
+    const receipt = await msgTx.wait();
+    console.log('waited for receipt: \n' + JSON.stringify(receipt.logs));
+    console.log('events: ' + JSON.stringify(receipt.events));
+
     const sequence = parseSequenceFromLogEth(
       receipt,
       CONTRACTS.DEVNET.ethereum.core,
     );
-    const emitterAddress = getEmitterAddressEth(
-      CONTRACTS.DEVNET.ethereum.token_bridge,
-    );
+    console.log('sequence: ' + sequence);
+    // const emitterAddress = getEmitterAddressEth(/*CONTRACTS.DEVNET.ethereum.core*/'0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0');
+    const emitterAddress = getEmitterAddressEth(signer.address);
+    console.log('emitter: ' + emitterAddress);
+
+    // const rpc = new GrpcWebImpl(ETH_RPC_URL, {});
+    // const api = new PublicRPCServiceClientImpl(rpc);
+    // const heart = await api.GetSignedVAA()
+
     // poll until the guardian(s) witness and sign the vaa
+    const chain = CHAINS['ethereum'];
+
     const { vaaBytes: signedVAA } = await getSignedVAAWithRetry(
       WORMHOLE_RPC_URLS,
-      CHAINS['ethereum'],
+      chain,
       emitterAddress,
       sequence,
       {
         transport: NodeHttpTransport(),
       },
     );
+    expect(signedVAA).not.toBeNull();
+    const parsed = parseVaa(signedVAA);
+    console.log('parsedVaa: ' + JSON.stringify(parsed.emitterChain));
+
     // create a keypair for Solana
     // const keypair = Keypair.fromSecretKey(SOLANA_PRIVATE_KEY);
     // const payerAddress = keypair.publicKey.toString();
@@ -178,114 +181,5 @@ describe('Test Wormhole ISM', () => {
   }, 1000000);
 });
 
-// // copypaste
-
-// export const CHAINS = {
-//   unset: 0,
-//   solana: 1,
-//   ethereum: 2,
-//   terra: 3,
-//   bsc: 4,
-//   polygon: 5,
-//   avalanche: 6,
-//   oasis: 7,
-//   algorand: 8,
-//   aurora: 9,
-//   fantom: 10,
-//   karura: 11,
-//   acala: 12,
-//   klaytn: 13,
-//   celo: 14,
-//   near: 15,
-//   moonbeam: 16,
-//   neon: 17,
-//   terra2: 18,
-//   injective: 19,
-//   osmosis: 20,
-//   sui: 21,
-//   aptos: 22,
-//   arbitrum: 23,
-//   optimism: 24,
-//   gnosis: 25,
-//   pythnet: 26,
-//   xpla: 28,
-//   btc: 29,
-//   base: 30,
-//   sei: 32,
-//   rootstock: 33,
-//   scroll: 34,
-//   wormchain: 3104,
-//   cosmoshub: 4000,
-//   evmos: 4001,
-//   kujira: 4002,
-//   neutron: 4003,
-//   celestia: 4004,
-//   sepolia: 10002,
-// } as const;
-
-// export async function getSignedVAA(
-//   host: string,
-//   emitterChain: ChainID,
-//   emitterAddress: string,
-//   sequence: string,
-//   extraGrpcOpts = {},
-// ) {
-//   const rpc = new GrpcWebImpl(host, extraGrpcOpts);
-//   const api = new PublicRPCServiceClientImpl(rpc);
-//   return await api.GetSignedVAA({
-//     messageId: {
-//       emitterChain,
-//       emitterAddress,
-//       sequence,
-//     },
-//   });
-// }
-
-// export async function getSignedVAAWithRetry(
-//   hosts: string[],
-//   emitterChain: ChainID,
-//   emitterAddress: string,
-//   sequence: string,
-//   extraGrpcOpts = {},
-//   retryTimeout = 1000,
-//   retryAttempts?: number,
-// ) {
-//   let currentWormholeRpcHost = -1;
-//   const getNextRpcHost = () => ++currentWormholeRpcHost % hosts.length;
-//   let result;
-//   let attempts = 0;
-//   while (!result) {
-//     attempts++;
-//     await new Promise((resolve) => setTimeout(resolve, retryTimeout));
-//     try {
-//       result = await getSignedVAA(
-//         hosts[getNextRpcHost()],
-//         emitterChain,
-//         emitterAddress,
-//         sequence,
-//         extraGrpcOpts,
-//       );
-//     } catch (e) {
-//       if (retryAttempts !== undefined && attempts > retryAttempts) {
-//         throw e;
-//       }
-//     }
-//   }
-//   return result;
-// }
-
-// export function parseSequencesFromLogEth(
-//     receipt: ContractReceipt,
-//     bridgeAddress: string
-// ): string[] {
-//   // TODO: dangerous!(?)
-//   const bridgeLogs = receipt.logs.filter((l) => {
-//     return l.address === bridgeAddress;
-//   });
-//   return bridgeLogs.map((bridgeLog) => {
-//     const {
-//       args: { sequence },
-//     } = Implementation__factory.createInterface().parseLog(bridgeLog);
-//     return sequence.toString();
-//   });
-// }
+// const getArbitraryBytes32 = (): string =>
+//   ethers.hexlify(ethers.toUtf8Bytes('asdfasdfasdf'));
