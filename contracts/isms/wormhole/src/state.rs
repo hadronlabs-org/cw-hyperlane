1b709451a7fb7c0b55e39c2460b6bbc67a7ff348
nhpd <dmitry.klpv@gmail.com>
2023-12-13 15:34:36 +0400

    use HexBinary instead of Vec<u8> inputs

--- contracts/isms/wormhole/src/state.rs
+++ contracts/isms/wormhole/src/state.rs
@@ -1,13 +1,17 @@
 use cosmwasm_schema::cw_serde;
+use cosmwasm_std::HexBinary;
 use cw_storage_plus::{Item, Map};
 
 #[cw_serde]
 pub struct Config {
     // TODO: comments
+    /// for wormhole verification
     pub emitter_chain: u16,
-    pub emitter_address: Vec<u8>,
+    pub emitter_address: HexBinary,
+
+    /// for hyperlane message verification
     pub origin_domain: u32,
-    pub origin_sender: Vec<u8>,
+    pub origin_sender: HexBinary,
 }
 
 /// **VERIFIED_IDS** contains all the `message.id`s (hash) that were verified by core wormhole contract
